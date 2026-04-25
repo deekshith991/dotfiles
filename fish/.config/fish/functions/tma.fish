dd8a2b404a96b66c851a84596228a936ca32c28f
Deekshith vaggu <vaggudeekshith@gmail.com>
2026-04-25 12:15:15 +0530

    ADD: uses current working dir as Session name  if no session name provided

--- fish/.config/fish/functions/tma.fish
+++ fish/.config/fish/functions/tma.fish
@@ -1,3 +1,9 @@
 function tma
-    tmux new-session -A -s (test (count $argv) -gt 0; and echo $argv[1]; or echo main)
+    set -l session (string split / (pwd))[-1]
+
+    if set -q argv[1]
+        set session $argv[1]
+    end
+
+    tmux new-session -A -s "dev-$session"
 end
