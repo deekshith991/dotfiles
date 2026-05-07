26bc542d12b155f17fce6612a16b37417db3b5f3
Deekshith vaggu <vaggudeekshith@gmail.com>
2026-05-07 17:20:32 +0530

    Ivy-rich added

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -132,3 +132,31 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0))
+
+;;; =========================
+;;; ivy-rich
+;;; =========================
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . cousel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history)))
+
+;;; ==========================
+;;; Helpful
+;;; ==========================
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
