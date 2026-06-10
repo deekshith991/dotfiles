a07231acf90446b8b16f643c3dbb18c170d2bf34
Deekshith vaggu <vaggudeekshith@gmail.com>
2026-06-10 14:50:58 +0530

    ADD(tuxedo): config for tuxedo
    We have new todo manager called tuxedo
    where we can save our todo's
    it's config file locations have been saved
    
    Changes to be committed:
    	modified:   fish/.config/fish/config.fish
    	modified:   fish/.config/fish/fish_variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,18 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 ~/.local/try.rb init ~/src/tries | source
+
+# # Start a new agent if the current one is invalid
+# if not set -q SSH_AUTH_SOCK; or not test -S "$SSH_AUTH_SOCK"
+#     eval (ssh-agent -c)
+# end
+#
+# # Add key if agent has no identities
+# ssh-add -l >/dev/null 2>&1
+# or ssh-add ~/.ssh/github_cachy_loq
+
+
+# tuxedo config
+set -x TODO_DIR "$HOME/notes"
+set -x TODO_FILE "$TODO_DIR/todo.txt"
+set -x DONE_FILE "$TODO_DIR/done.txt"
